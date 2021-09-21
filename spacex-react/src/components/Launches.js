40e19795acee8eca79ea3529c9761ad9d48e4f9d
Matt Moore <mmoore@catalyte.io>
2021-09-21 10:08:43 -0700

    Initial prototype

--- spacex-react/src/components/Launches.js
+++ spacex-react/src/components/Launches.js
@@ -36,35 +36,33 @@ export class Launches extends Component {
     return (
       <Fragment>
         <LandingPage />
-        <div className="launches">
-          <div className="container">
-            {/* <SearchBar onChange={this.onChange} /> */}
-            <div className="container-fluid">
-              <Query query={LAUNCHES_QUERY}>
-                {
-                  ({ loading, error, data }) => {
-                    if (loading) return <LoadingSpinner loading={loading} />
-                    if (error) console.log(error);
+        <div className="container" style={{minHeight:'100vh'}}>
+          <SearchBar onChange={this.onChange} />
+          <div className="container-fluid">
+            <Query query={LAUNCHES_QUERY}>
+              {
+                ({ loading, error, data }) => {
+                  if (loading) return <LoadingSpinner loading={loading} />
+                  if (error) console.log(error);
 
-                    const { search } = this.state;
-                    const filteredLaunch = data.launches.filter(launch => {
-                      return launch.mission_name.toLowerCase().includes(search.toLowerCase()) ||
-                            launch.launch_date_local.includes(search)
-                    })
-                    return (
-                      <div className="row">
-                        {
-                          filteredLaunch.filter(launch => !launch.upcoming)
-                            .map(launch => (
-                              <LaunchItem key={launch.flight_number} launch={launch} />
-                            ))
-                        }
-                      </div>
-                    )
-                  }
+                  const { search } = this.state;
+                  const filteredLaunch = data.launches.filter(launch => {
+                    return launch.mission_name.toLowerCase().includes(search.toLowerCase()) ||
+                           launch.launch_date_local.includes(search)
+                  })
+                  return (
+                    <div className="row">
+                      {
+                        filteredLaunch.filter(launch => !launch.upcoming)
+                          .map(launch => (
+                            <LaunchItem key={launch.flight_number} launch={launch} />
+                          ))
+                      }
+                    </div>
+                  )
                 }
-              </Query>
-            </div>
+              }
+            </Query>
           </div>
         </div>
       </Fragment>
